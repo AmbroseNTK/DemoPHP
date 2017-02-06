794bba3e51e0fbb87835e40e75bf1cd798d0a6bc
Nguyen Tuan Kiet <nguyentuankiet23031998@gmail.com>
2017-02-06 10:10:53 +0700

    Basic function

--- main.php
+++ main.php
@@ -5,7 +5,10 @@ $arr[1]='C';
 $arr[]='D';
 $arr['kiet']='tuan';
 $n=3;
-if ($n%2==0) {
+function oddOrEven($num){
+    return $num%2==0;
+}
+if (oddOrEven($n)) {
     echo("So $n la so chan");
 } else {
     echo("So $n la so le");
@@ -34,5 +37,32 @@ for ($i=0; $i<4; $i++) {
     echo("<br/>");
 }
 print_r($arr);
+echo("<br/>");
+foreach ($arr as $key => $value) {
+    echo("$key = $value");
+    echo('<br/>');
+}
+function kiem_tra()
+{
+    // bien tinh
+    static $a = 0;
+    $a++;
+    echo $a;
+}
+  
+kiem_tra();
+kiem_tra();
+
+function lonNhat($a,$b){
+    if($a>$b)
+    {
+        return $a;
+    }
+    else
+    {
+        return $b;
+    }
+}
+echo('<br/>'.lonNhat(2,lonNhat(4,7)).'<br/>');
 
 ?>
